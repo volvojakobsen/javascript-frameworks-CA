c3682314a333650fb8fdeabf9e8caa7d1957afed
volvojakobsen <91479420+volvojakobsen@users.noreply.github.com>
2023-03-29 02:27:20 +0200

    products reviews

--- web-shop-app/src/pages/productDetails/productDetails.jsx
+++ web-shop-app/src/pages/productDetails/productDetails.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect, useContext} from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../../context/shop-context";
+import "./productDetails.css";
 
 
 export const ProductDetails = () => {
@@ -34,7 +35,6 @@ export const ProductDetails = () => {
      getData();
    }, []);
  
- 
    if (isLoading) {
      return <div>Loading posts</div>;
    }
@@ -42,22 +42,69 @@ export const ProductDetails = () => {
    if (isError) {
      return <div>Error loading data</div>;
    }
-  console.log(singleProduct);
+  /*console.log(singleProduct);*/
+  console.log(singleProduct.reviews);
 
    return <>
     <div>
-        <h2>{singleProduct.title}</h2>
-        <div>
-            <img src={singleProduct.imageUrl} alt="" srcset="" />
-            <div>
-              <h3>{singleProduct.price}</h3>
-            </div>
-            <div>
-                <button onClick={() => removeFromCart(id.id)}> - </button>
-                <input value={cartItems[id.id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id.id)} />
-                <button onClick={() => addToCart(id.id)}> + </button>
+        <div className="singleItem">
+            <img src={singleProduct.imageUrl} className="singleProductImage" alt="" srcset="" />
+            <div className="details">
+               <h2>{singleProduct.title}</h2>
+              <div>
+                <h3>{singleProduct.price}</h3>
+              </div>
+              <h1>Rating: {singleProduct.rating} of 5</h1>
+              <div>
+                  <button onClick={() => removeFromCart(id.id)}> - </button>
+                  <input value={cartItems[id.id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id.id)} />
+                  <button onClick={() => addToCart(id.id)}> + </button>
+              </div>
+              <p>{singleProduct.description}</p>
+              <div>
+              <h4>reviews</h4>
+              {singleProduct.reviews && singleProduct.reviews.map(post =>{
+                return (
+                  <div className="singleReview" key={post.id}>
+                    <div className="name"><div>By: {post.username}</div>rated it: {post.rating} of 5.</div>
+                    <div className="comment">{post.description}</div>
+                  </div>
+                );
+
+              })};
+              </div>
+              
             </div>
         </div>
     </div>
     </>
 };
+
+/*
+<div className="reviews">
+                {singleProduct.reviews.map((singleProduct) => {
+                  if(singleProduct.reviews > 0) {
+                      return <Reviews data={singleProduct} />
+                  }
+                })}
+            </div>
+            */
+
+
+
+
+            /*
+
+            <div>
+              <h4>reviews</h4>
+              {singleProduct.reviews.map(post =>{
+                return (
+                  <div className="singleReview">
+                    <div className="name"><div>By: {post.username}</div>rated it: {post.rating} of 5.</div>
+                    <div className="comment">{post.description}</div>
+                  </div>
+                );
+
+              })};
+              </div>
+              */
